deff0cb3bab4e5d2b413d812f717014fa5ee924a
Onariaginosa Igbinedion <Ona.Igbinedion@gmail.com>
2018-08-08 10:42:52 -0700

    added some basic app functionality

---
+++ artist_service.py
@@ -0,0 +1,18 @@
+artist_info= {
+        'Drake':{
+            'image': 'Drake.png',
+            'bio': "Listen to Drake's music. It's decent, I guess",
+            'spotify':'https://open.spotify.com/embed/artist/3TVXtAsR1Inumwj472S9r4'
+        },
+        'Rihanna':{
+            'image':'Rihanna.png',
+            'bio':"Listen to Rihanna. She da best music!",
+            'spotify':'https://open.spotify.com/embed/artist/5pKCCKE2ajJHZ9KAiaK11H'
+        },
+        'Beyonce':{
+            'image':'Beyonce.png',
+            'bio': "Listen to Beyonce. They don't call her queen for nothing.",
+            'spotify':'https://open.spotify.com/embed/artist/6vWDO969PvNqNYHIOW5v0m'
+        },
+
+}
